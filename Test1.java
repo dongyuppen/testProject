348afa0d43f8609077fa4e9873702cf13bb81836
dongyuppen <dongyuppen@naver.com>
2025-03-27 19:25:45 +0900

    주석을 추가했습니다.

--- Test1.java
+++ Test1.java
@@ -1,5 +1,5 @@
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World"); //Hello World를 출력합니다.
     }
 }
